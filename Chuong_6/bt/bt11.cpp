c119d2e09e957fc36164a24bd02bbafc106b204a
thiendao1407 <53300329+thiendao1407@users.noreply.github.com>
2019-07-25 21:53:03 +0700

    sua bai tap 11

--- Chuong_6/bt/bt11.cpp
+++ Chuong_6/bt/bt11.cpp
@@ -6,21 +6,17 @@ int main()
 	printf("Nhap be ngang: \n"); scanf("%d", &ngang);
 	
 	for(j=1;j<=ngang;j++)
-			printf("*"); printf("\n"); 
+		printf("*"); 
+	printf("\n"); 
 	for(i=1;i<=doc-2;i++)
 	{
-		for(j=1;j<=ngang;j++)
+		printf("*");
+		for(int g=1; g<=ngang-2;g++)
 		{
-			if(j==1) printf("*");
-			else if (j<ngang) printf(" ");
-			else 
-			{
-			printf("*");	
-			printf("\n");
-			}
+			printf(" ");
 		}
+		printf("*\n");
 	}
-	
 	for(j=1;j<=ngang;j++)
 		printf("*");	
 }
