4d1cad2d96ac7514ef97816b958409d7e9d728e9
thiendao1407 <dovantuan.ftu@gmail.com>
2019-07-26 22:35:48 +0700

    sua bai 10 chuong 6

--- Chuong_6/bt/bt10.cpp
+++ Chuong_6/bt/bt10.cpp
@@ -1,28 +1,22 @@
 #include<stdio.h>
 int main()
 {
-int n,i,j;
+int n;
     printf("nhap chieu cao tam giac:");
     scanf("%d",&n);
     printf("\n");
-    for(i=1;i<n;i++)
+    for(int j=1;j<n;j++)
+    	printf(" ");
+        printf("*\n");
+    for(int i=1;i<n-1;i++)
     {
-       for(j=1;j<=n-i;j++)
-           printf(" ");
-       for(j=1;j<=2*i-1;j++)
-       {
-         if (j==1||j==2*i-1)
-             printf("*");
-         else 
-             printf (" ");
-       }
-       printf("\n"); 
-       if (i==n-1)
-       {  
-          for(j=1; j<=2*n-1;j++) 
-               printf ("*");
-          break ;
-       
-       }
-    }
+    	for(int j=1;j<n-i;j++)
+    	printf(" ");
+    	printf("*");
+    	for(int j=1;j<2*i;j++)
+    	printf(" ");
+    	printf("*\n");
+	}
+	for(int j=1;j<2*n;j++)
+		printf("*");
 }
