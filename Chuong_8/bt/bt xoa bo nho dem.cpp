c33121d3c832e1c1a82700292d6a0562fc976788
thiendao1407 <dovantuan.ftu@gmail.com>
2019-08-03 20:29:54 +0700

    ham xoa bo nho dem

--- Chuong_8/bt/bt xoa bo nho dem.cpp
+++ Chuong_8/bt/bt xoa bo nho dem.cpp
@@ -1,17 +1,8 @@
-#include<stdio.h>
+// Ham xoa bo nho dem
 
+#include<stdio.h>
 void xoabonhodem();
 void xoabonhodem() {
   char c[50];
   gets(c);
-}
-
-main() {
-  int i = 0;
-  do {
-    printf("Nhap mot so duong: ");
-    scanf("%d", & i);
-    xoabonhodem();
-  } while (i < 1);
-  printf("%d", i);
 }
